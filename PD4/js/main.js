c0ba25359cb81a929de3d6b307cf44a99b4575ae
CyberLunarion <Lunarion035@gmail.com>
2023-11-09 11:16:15 +0200

    PD4 Exercise Done

---
+++ PD4/js/main.js
@@ -0,0 +1,9 @@
+let petName;
+let petAge;
+petName = 'Rex';
+petAge = 5;
+console.log(petName, petAge);
+petName = 'Buddy';
+petAge = 4;
+const buildingAddress = 'Trinapolio 2';
+console.log(buildingAddress);
