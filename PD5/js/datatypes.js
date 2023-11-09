1e959674835a4ce9e5d2ed3852087f9be0cbb1f1
CyberLunarion <Lunarion035@gmail.com>
2023-11-09 12:45:58 +0200

    PD5 Exercise Done

---
+++ PD5/js/datatypes.js
@@ -0,0 +1,42 @@
+let numbers = 15;
+console.log(numbers);
+let bigInt = BigInt('185667403670566385649236585');
+console.log(bigInt);
+let string = 'Hello!';
+console.log(string);
+let boolean = true;
+console.log(boolean);
+let notdefined;
+console.log(notdefined);
+let nothing = null;
+console.log(nothing);
+let fox = {
+    name:'Fox', age:9, height:78, color:'Orange, Black and White'
+};
+console.log(fox);
+
+let number = 12;
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isTrue = (true == false);
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+    humanName: undefined,
+    humanAge: null
+};
+console.log(typeof(number));
+console.log(typeof(otherNumber));
+console.log(typeof(textOne));
+console.log(typeof(textTwo));
+console.log(typeof(textThree));
+console.log(typeof(isTrue));
+console.log(typeof(isEqual));
+console.log(typeof(petName));
+console.log(typeof(petAge));
+console.log(typeof(human));
+console.log(typeof(humanName));
+console.log(typeof(humanAge));
