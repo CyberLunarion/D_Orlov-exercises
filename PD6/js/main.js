c563450f38b861590e5d2aff773d8b7197b21010
Dmitrij <Dmitrij.Orlov@stud.vtmc.lt>
2023-11-16 10:26:09 +0200

    PD6 Exercise Done

---
+++ PD6/js/main.js
@@ -0,0 +1,40 @@
+let funnyNumber = 12345; //Convert to string
+let funnyString = '12345'; //Convert to number
+let funnyTrue = '1'; // Convert to Boolean
+let funnyFalse = '0'; // Convert to Boolean
+let isTrue = false; // Convert to String
+let isFalse = true; // Convert to Number
+let funnyText = 'JavaScript is dynamic programming language.'; // Convert to Number
+let funnyNull = null; // Convert to String
+let funnyUndefined = undefined; // Convert to String
+let otherIsTrue = true; // Convert to String
+let otherIsFalse = false; // Convert to String
+let booleanString = 'true'; // Convert to Number
+let otherBooleanString = 'false'; // Convert to Boolean
+
+funnyNumber = '12345';
+console.log(typeof(funnyNumber));
+funnyString = 12345;
+console.log(typeof(funnyString));
+funnyTrue = true;
+console.log(typeof(funnyTrue));
+funnyFalse = false;
+console.log(typeof(funnyFalse));
+isTrue = 'true';
+console.log(typeof(isTrue));
+isFalse = 0;
+console.log(typeof(isFalse));
+funnyText = 98765;
+console.log(typeof(funnyText));
+funnyNull = 'null';
+console.log(typeof(funnyNull));
+funnyUndefined = "undefined";
+console.log(typeof(funnyUndefined));
+otherIsTrue = 'true';
+console.log(typeof(otherIsTrue));
+otherIsFalse = "false";
+console.log(typeof(otherIsFalse));
+booleanString = 1;
+console.log(typeof(booleanString));
+otherBooleanString = false;
+console.log(typeof(otherBooleanString));
